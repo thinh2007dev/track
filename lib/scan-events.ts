b01a3c603cfb65d0d18ee7ac0e173b8494551139
thinh2007dev <minhthinhhuynh38@gmail.com>
2026-09-09 10:53:48 +0700

    Clear again

--- lib/scan-events.ts
+++
@@ -1,6 +0,0 @@
-import { EventEmitter } from "node:events";
-
-const globalEvents = globalThis as unknown as { fruitVaultEvents?: EventEmitter };
-export const scanEvents = globalEvents.fruitVaultEvents ?? new EventEmitter();
-globalEvents.fruitVaultEvents = scanEvents;
-scanEvents.setMaxListeners(100);
